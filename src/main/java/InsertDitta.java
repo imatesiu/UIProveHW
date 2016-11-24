f76542ee22f8c91c67205d3609a402386300a275
Giorgio <imatesiu@gmail.com>
2016-11-24 20:10:08 +0100

    insert form added

--- src/main/java/InsertDitta.java
+++ src/main/java/InsertDitta.java
@@ -1,16 +1,49 @@
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
+
 @ManagedBean
+@RequestScoped
 public class InsertDitta {
 	
+	private String nomeDitta;
+	private String indirizzo;
+	private String pIVA;
 	
 	
+	
+	
+	
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public String getNomeDitta() {
+		return nomeDitta;
+	}
+
+	public void setNomeDitta(String nomeDitta) {
+		this.nomeDitta = nomeDitta;
+	}
+
+	public String getpIVA() {
+		return pIVA;
+	}
+
+	public void setpIVA(String pIVA) {
+		this.pIVA = pIVA;
+	}
+
 	public void save(ActionEvent actionEvent) {
-        addMessage("Data saved");
+        addMessage("Data saved ditta");
     }
      
     public void close(ActionEvent actionEvent) {
--- src/main/java/InsertMF.java
+++ src/main/java/InsertMF.java
@@ -1,24 +1,78 @@
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 
 @ManagedBean
+@RequestScoped
 public class InsertMF {
-	
-	
-	
-	public void save(ActionEvent actionEvent) {
-        addMessage("Data saved");
-    }
-     
-    public void close(ActionEvent actionEvent) {
-        addMessage("Data updated");
-    }
 
-    public void addMessage(String summary) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
-        FacesContext.getCurrentInstance().addMessage(null, message);
-    }
+	private List<SelectItem> ListDitta;
+	private String nomeDispositivo;
+	private String nomeDitta;
+	private String numeroRapportoProva;
+
+
+	@PostConstruct
+	public void init() {
+		//cars
+
+		SelectItem[] items = new SelectItem[] {new SelectItem("BMW", "BMW"), new SelectItem("Mercedes", "Mercedes"), new SelectItem("Volkswagen", "Volkswagen")};
+		ListDitta = new ArrayList<SelectItem>(Arrays.asList(items));
+		
+	}
+
+	
+	public String getNomeDitta() {
+		return nomeDitta;
+	}
+
+
+	public void setNomeDitta(String nomeDitta) {
+		this.nomeDitta = nomeDitta;
+	}
+
+
+	public String getNomeDispositivo() {
+		return nomeDispositivo;
+	}
+
+	public void setNomeDispositivo(String nomeDispositivo) {
+		this.nomeDispositivo = nomeDispositivo;
+	}
+
+	public String getNumeroRapportoProva() {
+		return numeroRapportoProva;
+	}
+
+	public void setNumeroRapportoProva(String numeroRapportoProva) {
+		this.numeroRapportoProva = numeroRapportoProva;
+	}
+
+	public List<SelectItem> getDitta() {
+		return ListDitta;
+	}
+
+	public void save(ActionEvent actionEvent) {
+		addMessage("Data saved");
+	}
+
+	public void close(ActionEvent actionEvent) {
+		addMessage("Data updated");
+	}
+
+	public void addMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
 }
