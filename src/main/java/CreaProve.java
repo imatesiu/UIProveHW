cd6897b1fe1749b773563bab1fcf91a74558ac89
Giorgio <imatesiu@gmail.com>
2016-11-24 22:05:04 +0100

    crea prove

--- src/main/java/CreaProve.java
+++ src/main/java/CreaProve.java
@@ -4,19 +4,64 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
+import org.primefaces.event.DragDropEvent;
+import javax.faces.bean.ViewScoped;
+
 @ManagedBean
-@RequestScoped
+@ViewScoped
 public class CreaProve {
 
 	private String nomeDitta;
 	private String modelloMF;
 	private String numeroRapportoProva;
 	
+	@ManagedProperty("#{carService}")
+    private CarService service;
+
+    private List<Prova> prove;
+    
+    private List<Prova> droppedProve;
+    
+    private Prova selectedProve;
+    
+    @PostConstruct
+    public void init() {
+        prove = service.createCars(9);
+        droppedProve = new ArrayList<Prova>();
+    }
+    
+    public void onProveDrop(DragDropEvent ddEvent) {
+        Prova car = ((Prova) ddEvent.getData());
+ 
+        droppedProve.add(car);
+        prove.remove(car);
+    }
+    
+    public void setService(CarService service) {
+        this.service = service;
+    }
+
+    public List<Prova> getProve() {
+        return prove;
+    }
+
+    public List<Prova> getDroppedProve() {
+        return droppedProve;
+    }    
+
+    public Prova getSelectedProva() {
+        return selectedProve;
+    }
+
+    public void setSelectedProve(Prova selectedProve) {
+        this.selectedProve = selectedProve;
+    }
 	
 	  public List<String> completeModelloMF(String query) {
 	        List<String> allThemes =  new ArrayList<String>();
