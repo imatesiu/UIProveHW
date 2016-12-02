fce6835e4e9549bf9a7fd7c68272661fdf05b576
Giorgio <imatesiu@gmail.com>
2016-12-02 23:42:44 +0100

    stat added

---
+++ src/main/java/Statistics.java
@@ -0,0 +1,152 @@
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.BubbleChartModel;
+import org.primefaces.model.chart.BubbleChartSeries;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.HorizontalBarChartModel;
+
+@ManagedBean
+@ViewScoped
+public class Statistics {
+
+	private BubbleChartModel bubbleModel;
+	private BarChartModel barModel;
+	private HorizontalBarChartModel horizontalBarModel;
+
+
+
+	@PostConstruct
+	public void init() {
+		createBubbleModels();
+		createBarModels();
+	}
+
+	public BubbleChartModel getBubbleModel() {
+		return bubbleModel;
+	}
+
+
+	private void createBubbleModels(){
+		bubbleModel = initBubbleModel();
+		bubbleModel.setTitle("Bubble Chart");
+		bubbleModel.getAxis(AxisType.X).setLabel("Price");
+		Axis yAxis = bubbleModel.getAxis(AxisType.Y);
+		yAxis.setMin(0);
+		yAxis.setMax(250);
+		yAxis.setLabel("Labels");
+
+
+	}
+
+	private BubbleChartModel initBubbleModel(){
+		BubbleChartModel model = new BubbleChartModel();
+
+		model.add(new BubbleChartSeries("Acura", 70, 183,55));
+		model.add(new BubbleChartSeries("Alfa Romeo", 45, 92, 36));
+		model.add(new BubbleChartSeries("AM General", 24, 104, 40));
+		model.add(new BubbleChartSeries("Bugatti", 50, 123, 60));
+		model.add(new BubbleChartSeries("BMW", 15, 89, 25));
+		model.add(new BubbleChartSeries("Audi", 40, 180, 80));
+		model.add(new BubbleChartSeries("Aston Martin", 70, 70, 48));
+
+		return model;
+	}
+
+
+
+
+
+	public BarChartModel getBarModel() {
+		return barModel;
+	}
+
+	public HorizontalBarChartModel getHorizontalBarModel() {
+		return horizontalBarModel;
+	}
+
+	private BarChartModel initBarModel() {
+		BarChartModel model = new BarChartModel();
+
+		ChartSeries boys = new ChartSeries();
+		boys.setLabel("Boys");
+		boys.set("2004", 120);
+		boys.set("2005", 100);
+		boys.set("2006", 44);
+		boys.set("2007", 150);
+		boys.set("2008", 25);
+
+		ChartSeries girls = new ChartSeries();
+		girls.setLabel("Girls");
+		girls.set("2004", 52);
+		girls.set("2005", 60);
+		girls.set("2006", 110);
+		girls.set("2007", 135);
+		girls.set("2008", 120);
+
+		model.addSeries(boys);
+		model.addSeries(girls);
+
+		return model;
+	}
+
+	private void createBarModels() {
+		createBarModel();
+		createHorizontalBarModel();
+	}
+
+	private void createBarModel() {
+		barModel = initBarModel();
+
+		barModel.setTitle("Bar Chart");
+		barModel.setLegendPosition("ne");
+
+		Axis xAxis = barModel.getAxis(AxisType.X);
+		xAxis.setLabel("Gender");
+
+		Axis yAxis = barModel.getAxis(AxisType.Y);
+		yAxis.setLabel("Births");
+		yAxis.setMin(0);
+		yAxis.setMax(200);
+	}
+
+	private void createHorizontalBarModel() {
+		horizontalBarModel = new HorizontalBarChartModel();
+
+		ChartSeries boys = new ChartSeries();
+		boys.setLabel("Boys");
+		boys.set("2004", 50);
+		boys.set("2005", 96);
+		boys.set("2006", 44);
+		boys.set("2007", 55);
+		boys.set("2008", 25);
+
+		ChartSeries girls = new ChartSeries();
+		girls.setLabel("Girls");
+		girls.set("2004", 52);
+		girls.set("2005", 60);
+		girls.set("2006", 82);
+		girls.set("2007", 35);
+		girls.set("2008", 120);
+
+		horizontalBarModel.addSeries(boys);
+		horizontalBarModel.addSeries(girls);
+
+		horizontalBarModel.setTitle("Horizontal and Stacked");
+		horizontalBarModel.setLegendPosition("e");
+		horizontalBarModel.setStacked(true);
+
+		Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
+		xAxis.setLabel("Births");
+		xAxis.setMin(0);
+		xAxis.setMax(200);
+
+		Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
+		yAxis.setLabel("Gender");        
+	}
+
+}
