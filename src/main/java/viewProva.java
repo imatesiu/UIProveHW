de65b1375be5b313abf81a3fa44dd3721a3f5d52
Giorgio <imatesiu@gmail.com>
2016-12-02 22:36:46 +0100

    bug fix

--- src/main/java/viewProva.java
+++ src/main/java/viewProva.java
@@ -10,6 +10,8 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -22,7 +24,7 @@ import isti.cnr.sse.rest.data.Esito;
 import isti.cnr.sse.rest.data.Prova;
 
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class viewProva {
 
 	private Prova prova = new Prova();
