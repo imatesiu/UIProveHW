f4e4cd8a781fee4fa6aaaa47636f54f2eb2438ba
Giorgio <imatesiu@gmail.com>
2016-12-04 19:26:25 +0100

    update converter

--- src/main/java/isti/cnr/sse/rest/data/ModelloMFConverter.java
+++ src/main/java/isti/cnr/sse/rest/data/ModelloMFConverter.java
@@ -7,41 +7,49 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 
+import isti.cnr.sse.SelectProve;
+
 
 //To be considered by the JavaServer Faces As Managed Bean
 // & Make the converter Eligible for use @ManagedProperty
 @ManagedBean
-@RequestScoped
+//@RequestScoped
 //@FacesConverter
 public class ModelloMFConverter implements Converter{
-	
-	  @ManagedProperty(value="#{ditta}")
-		private Ditta ditta;
 
 
-	public Ditta getDitta() {
-		return ditta;
-	}
-
-	public void setDitta(Ditta ditta) {
-		this.ditta = ditta;
-	}
 
 	@Override
-	public Object getAsObject(FacesContext context, UIComponent component,String value) {
-		for(ModelloMF p : ditta.getMisuratoriFiscali()){
-			if(p.getNomeModello().equals(value)){
-				return p;
-			}
-		}
-		return null;
+	public Object getAsObject(FacesContext fc, UIComponent component,String value) {
+
+
+		// SelectProve service = (SelectProve) fc.getELContext().getELResolver().getValue(fc.getELContext(), null,"selectProve");
+	//	SelectProve service = (SelectProve) fc.getExternalContext().getApplicationMap().get("selectProve");
+	//	Ditta ditta = (Ditta)  FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("ditta");
+		 SelectProve service = (SelectProve) fc.getApplication()
+				    .getVariableResolver().resolveVariable(fc, "selectProve");
+
+			Ditta ditta = service.getDitta();
+			if(ditta!=null)
+				for(ModelloMF p : ditta.getMisuratoriFiscali()){
+					if(p.getNomeModello().equals(value)){
+						return p;
+					}
+				}
+			return null;
+
+
+	
+
+
+
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component,Object value) {
 		if(value instanceof ModelloMF){
 			ModelloMF player = (ModelloMF)value;
-			ditta.getMisuratoriFiscali().add(player);
+			//ditta.getMisuratoriFiscali().add(player);
 			return player.getNomeModello();
 		}
 		return "";
